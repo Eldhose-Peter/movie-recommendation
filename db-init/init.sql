2c606128d05f064204d4639d8c3a877e89215d38
Eldhose-Peter <eldhosepeter2000@gmail.com>
2024-10-31 19:17:56 +0530

    seeding db

--- db-init/init.sql
+++ db-init/init.sql
@@ -10,3 +10,39 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create the movies table
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    year INT,
+    country TEXT,
+    genre TEXT,
+    director TEXT,
+    minutes INT,
+    poster TEXT
+);
+
+-- Load the CSV data into the movies table
+COPY movies(id, title, year, country, genre, director, minutes, poster)
+FROM '/docker-entrypoint-initdb.d/ratedmoviesfull.csv' -- replace with your actual file path
+DELIMITER ','
+CSV HEADER;
+
+-- Create the ratings table
+CREATE TABLE ratings (
+    rater_id INT,
+    movie_id VARCHAR(20),
+    rating INT NOT NULL,
+    time TEXT,
+    PRIMARY KEY (rater_id, movie_id)
+);
+
+ALTER TABLE ratings
+ADD CONSTRAINT ratings_rating_check CHECK (rating >= 0 AND rating <= 10); 
+
+-- Load the CSV data into the ratings table
+COPY ratings(rater_id, movie_id, rating, time)
+FROM '/docker-entrypoint-initdb.d/ratings.csv' -- replace with your actual file path
+DELIMITER ','
+CSV HEADER;
+
